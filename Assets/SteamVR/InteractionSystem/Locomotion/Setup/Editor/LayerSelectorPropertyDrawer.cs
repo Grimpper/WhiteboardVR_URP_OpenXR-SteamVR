1cbe1bbefae7587adb6fea12854fed6e6720bd2d
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2021-03-11 01:33:26 +0100

    Fixed ResetLeyer lacking No change option for layer selector & played with the camera renderer to paint on a texture

--- Assets/SteamVR/InteractionSystem/Locomotion/Setup/Editor/LayerSelectorPropertyDrawer.cs
+++ Assets/SteamVR/InteractionSystem/Locomotion/Setup/Editor/LayerSelectorPropertyDrawer.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 [CustomPropertyDrawer(typeof(LayerSelectorAtributte))]
 public class LayerSelectorPropertyDrawer : AbstractSelectorPropertyAtributte
 {
+    protected override string getVoidSelectionValue()
+    {
+        return "<No change>";
+    }
     protected override string[] getItemList()
     {
         return UnityEditorInternal.InternalEditorUtility.layers;
