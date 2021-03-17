a155c98cddfcfe1e021c64727701f28efd3488ed
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2021-03-17 17:31:08 +0100

    Improved BoardPoint collector Debug Log Added boxcollider to change contact offset. Now is a parameter

--- Assets/Scripts/Whiteboard/BoardPointCollector.cs
+++ Assets/Scripts/Whiteboard/BoardPointCollector.cs
@@ -9,9 +9,10 @@ public class BoardPointCollector : MonoBehaviour
 {
     // Parameters
     [SerializeField] [TagSelectorAtributte] private string markerTag;
+    [SerializeField] private float contactOffset = 0.01f;
     [SerializeField] private float sampleRate = 1f;
     [SerializeField] private bool showDebug = false;
-    
+
     // Raycast variables
     public RaycastHit raycastHit;
     private float raycastOffset = 0.1f;
@@ -21,21 +22,46 @@ public class BoardPointCollector : MonoBehaviour
     private IEnumerator samplePoints;
     private BoardRenderer boardRenderer;
     private Collision markerCollision = new Collision();
-
+    
+    // Original collided object variables
+    private float originalContactOffset;
+    
     private void Start()
     {
+        MeshCollider boardCollider = GetComponent<MeshCollider>();
+        boardCollider.contactOffset = contactOffset;
         boardRenderer = GetComponent<BoardRenderer>();
         layerMask = (int)Mathf.Pow(2f, gameObject.layer);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag(markerTag)) return;
+        
+        originalContactOffset = other.contactOffset;
+        other.contactOffset = contactOffset;
+        
+        if(showDebug) Debug.Log("Triggered: changing contactOffset");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag(markerTag)) return;
+        
+        other.contactOffset = originalContactOffset;
+        
+        if(showDebug) Debug.Log("Exited trigger: restoring contactOffset");
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (!other.collider.gameObject.CompareTag(markerTag)) return;
-        
+       
         if (samplePoints != null) StopCoroutine(samplePoints);
-
         samplePoints = SamplePointsRoutine();
         StartCoroutine(samplePoints);
+        
+        if(showDebug) Debug.Log("Collided: starting sampling coroutine");
     }
 
     private void OnCollisionStay(Collision other)
@@ -43,6 +69,8 @@ public class BoardPointCollector : MonoBehaviour
         if (!other.collider.gameObject.CompareTag(markerTag)) return;
 
         markerCollision = other;
+        
+        if(showDebug) Debug.Log("Colliding: updating collision");
     }
 
 
@@ -52,6 +80,8 @@ public class BoardPointCollector : MonoBehaviour
          
          StopCoroutine(samplePoints);
          boardRenderer.StrokeCleared = true;
+         
+         if(showDebug) Debug.Log("Decollided: stopping sampling coroutine");
      }
 
     private Vector3 GetAverageCollisionPoint(Collision collision)
@@ -92,23 +122,23 @@ public class BoardPointCollector : MonoBehaviour
 
              float rayLength = Mathf.Infinity;
              Ray ray = new Ray(raycastOrigin, collisionAverageNormal);
-             if (Physics.Raycast(ray, out raycastHit, rayLength, layerMask))
+             if (Physics.Raycast(ray, out raycastHit, rayLength, layerMask, QueryTriggerInteraction.Ignore))
              {
                  boardRenderer.SetColor(markerCollision.gameObject.GetComponent<Marker>().Color);
                  boardRenderer.CollisionHit = raycastHit;
                  
                  if (showDebug)
                  {
-                     Debug.Log("MARKER COLOR: " + markerCollision.gameObject.GetComponent<Marker>().Color);
+                     Debug.Log("Marker Color: " + markerCollision.gameObject.GetComponent<Marker>().Color);
                      
                      Debug.DrawRay(ray.origin,ray.direction, Color.green, duration: 10000, false);
-                     Debug.Log("RAYCAST LAYER MASK: " + layerMask);
-                     Debug.Log("RAYCAST POINT: " + raycastHit.point);
-                     Debug.Log("RAYCAST TEXCOORD: " + raycastHit.textureCoord);
+                     Debug.Log("Raycast Layer Mask: " + layerMask);
+                     Debug.Log("Raycast Hit Point: " + raycastHit.point);
+                     Debug.Log("Raycast TextCoord: " + raycastHit.textureCoord);
                  }
                  
              }
-
+             
              yield return new WaitForSeconds(sampleRate);
          }
      }
